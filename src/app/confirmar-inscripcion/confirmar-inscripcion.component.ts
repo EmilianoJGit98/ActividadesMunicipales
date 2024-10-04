fa4e8c9c64d8aea530784c9ca0012b2c14811be3
EmilianoJGit98 <ejg250398@gmail.com>
2024-10-04 08:58:43 -0300

    nota

--- src/app/confirmar-inscripcion/confirmar-inscripcion.component.ts
+++ src/app/confirmar-inscripcion/confirmar-inscripcion.component.ts
@@ -1,12 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';  
-import { CommonModule } from '@angular/common';  
-import { Router, RouterModule, ActivatedRoute } from '@angular/router';  
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 import { dataRegistro } from '../servicios/data-registro.service';
-import Swal from 'sweetalert2'; // Si no lo has importado, asegúrate de hacerlo 
+import Swal from 'sweetalert2'; // Si no lo has importado, asegúrate de hacerlo
 import { PagoPersonalizadoService } from '../servicios/pagoPersonalizado.service';
 import { HttpClient } from '@angular/common/http';
-
-
 
 interface IParticipante {
   nombre: string;
@@ -18,9 +16,9 @@ interface IParticipante {
   telefono: number;
 }
 interface IItems {
-   id: string
-   unit_price: number
-   title: string 
+  id: string;
+  unit_price: number;
+  title: string;
 }
 declare var MercadoPago: any;
 @Component({
@@ -29,12 +27,12 @@ declare var MercadoPago: any;
   imports: [CommonModule, RouterModule],
   templateUrl: './confirmar-inscripcion.component.html',
   styleUrls: ['./confirmar-inscripcion.component.css'],
-  providers: [], 
+  providers: [],
 })
 export class ConfirmarInscripcionComponent implements OnInit {
   IDevento: number = 0;
   totalMonto: number = 0;
-  dataObject:any;
+  dataObject: any;
   publicKey = 'APP_USR-9447b8bb-b859-4dcb-828d-fe47d345edfc'; // Asigna tu clave pública
   locale = 'es-AR'; // O el idioma que desees
   paymentId: number | 0 = 0;
@@ -49,6 +47,8 @@ export class ConfirmarInscripcionComponent implements OnInit {
     cuit: 0,
   };
 
+  //nota
+
   ArrActividades: IItems[] = [];
 
   constructor(
@@ -56,8 +56,8 @@ export class ConfirmarInscripcionComponent implements OnInit {
     private route: ActivatedRoute,
     private dataRegistroService: dataRegistro,
     private pagoPersonalizado: PagoPersonalizadoService
-    // private formBuilder: FormBuilder
-  ) {}
+  ) // private formBuilder: FormBuilder
+  {}
 
   ngOnInit(): void {
     const formDataString = localStorage.getItem('formRegistroData');
@@ -67,7 +67,6 @@ export class ConfirmarInscripcionComponent implements OnInit {
       // console.log(JSON.stringify(this.formRegistroData));
     }
 
-    
     // Suscribirse a los datos del servicio
     this.dataRegistroService.currentFormData.subscribe((data) => {
       if (data) {
@@ -90,17 +89,17 @@ export class ConfirmarInscripcionComponent implements OnInit {
       this.participanteId = params['idContribuyente'];
       this.IDevento = params['IDevento'];
       this.totalMonto = params['totalMonto'];
-      this.ArrActividades = params['actividadesSeleccionadas'] ? JSON.parse(params['actividadesSeleccionadas']) : null;
+      this.ArrActividades = params['actividadesSeleccionadas']
+        ? JSON.parse(params['actividadesSeleccionadas'])
+        : null;
       this.formRegistroData;
-      
 
       // Verificar si paymentId existe
       if (this.paymentId) {
-       
-      console.log('Payment ID recibido:', this.paymentId);
-      console.log('conbtribuyente:', this.participanteId);
-      this.checkPayment(this.paymentId, this.participanteId);
-      this.router.navigate(['/pago-aprobado/'+this.IDevento]);
+        console.log('Payment ID recibido:', this.paymentId);
+        console.log('conbtribuyente:', this.participanteId);
+        this.checkPayment(this.paymentId, this.participanteId);
+        this.router.navigate(['/pago-aprobado/' + this.IDevento]);
       } else {
         console.log('No se encontró el payment_id.');
       }
@@ -144,44 +143,42 @@ export class ConfirmarInscripcionComponent implements OnInit {
   cargando: boolean = false;
   OcultaBoton: boolean = false;
 
-  putPago(){  
+  putPago() {
     this.cargando = true;
 
-    this.dataObject= {
+    this.dataObject = {
       participante: {
-        nombre : this.formRegistroData.nombre,
-        apellido : this.formRegistroData.apellido,
-        dni : this.formRegistroData.dni,
-        cuit : this.formRegistroData.cuit,
-        mail : this.formRegistroData.correo,
-        codigoArea : this.formRegistroData.cod_area,
-        numero : this.formRegistroData.telefono,
+        nombre: this.formRegistroData.nombre,
+        apellido: this.formRegistroData.apellido,
+        dni: this.formRegistroData.dni,
+        cuit: this.formRegistroData.cuit,
+        mail: this.formRegistroData.correo,
+        codigoArea: this.formRegistroData.cod_area,
+        numero: this.formRegistroData.telefono,
       },
-      items : this.ArrActividades
-    }
-  
-    this.pagoPersonalizado.enviarPago(this.dataObject).subscribe({  
-      next: (res: any) => {  
-        // console.log('Respuesta recibida:', res);  
+      items: this.ArrActividades,
+    };
+
+    this.pagoPersonalizado.enviarPago(this.dataObject).subscribe({
+      next: (res: any) => {
+        // console.log('Respuesta recibida:', res);
         this.participanteId = res.participante;
-      console.log('id Participante:', this.participanteId);
+        console.log('id Participante:', this.participanteId);
 
         this.createCheckoutButton(res.id);
-        
+
         // this.router.navigate(['/pago-aprobado/'+this.IDevento]);
-      },  
-      complete: () => {  
-        console.log('Datos enviados correctamente.');  
+      },
+      complete: () => {
+        console.log('Datos enviados correctamente.');
         this.cargando = false;
-
-      },  
-      error: (error: any) => {  
-        // console.error('Error al enviar datos', error); 
-        this.router.navigate(['/pago-rechazado/'+this.IDevento]); 
+      },
+      error: (error: any) => {
+        // console.error('Error al enviar datos', error);
+        this.router.navigate(['/pago-rechazado/' + this.IDevento]);
         this.cargando = false;
-
-      }  
-    });  
+      },
+    });
   }
   async createCheckoutButton(preferenceId: string): Promise<void> {
     const mercadopago = new MercadoPago(this.publicKey, {
@@ -192,15 +189,18 @@ export class ConfirmarInscripcionComponent implements OnInit {
     await this.renderComponent(bricksBuilder, preferenceId);
   }
 
-
-  private async renderComponent(bricksBuilder: any, preferenceId: string): Promise<void> {
+  private async renderComponent(
+    bricksBuilder: any,
+    preferenceId: string
+  ): Promise<void> {
     if (window.checkoutButton) window.checkoutButton.unmount();
     await bricksBuilder.create('wallet', 'formActividadbtn', {
       initialization: {
         preferenceId: preferenceId,
       },
       callbacks: {
-        onError: (error: any) => console.error('Error en el botón de pago:', error),
+        onError: (error: any) =>
+          console.error('Error en el botón de pago:', error),
         onReady: () => {
           console.log('Botón de pago listo');
           this.OcultaBoton = true;
@@ -209,22 +209,23 @@ export class ConfirmarInscripcionComponent implements OnInit {
     });
   }
   private async checkPayment(paymentId: number, participanteId: number) {
-  
     // Verificar si la conversión fue exitosa
     if (!isNaN(this.paymentId)) {
       this.participanteId = participanteId;
-      this.pagoPersonalizado.getPayment(this.paymentId, this.participanteId).subscribe({  
-        next: (res: any) => {  
-          console.log('Respuesta payment:', res);  
-          console.log('participante:'+this.participanteId)
-        },  
-        complete: () => {  
-          console.log('Datos enviados correctamente.');  
-        },  
-        error: (error: any) => {  
-          console.error('Error al enviar datos', error);  
-        }  
-      });
+      this.pagoPersonalizado
+        .getPayment(this.paymentId, this.participanteId)
+        .subscribe({
+          next: (res: any) => {
+            console.log('Respuesta payment:', res);
+            console.log('participante:' + this.participanteId);
+          },
+          complete: () => {
+            console.log('Datos enviados correctamente.');
+          },
+          error: (error: any) => {
+            console.error('Error al enviar datos', error);
+          },
+        });
     } else {
       console.error('paymentId no es un número válido:', paymentId);
       // console.log("mercado pago ejecutad")
